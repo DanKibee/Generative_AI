7615be5913340679602eae254ef833a4d7ac528f
Daniel Kibe <148968967+DanKibee@users.noreply.github.com>
2024-11-01 17:57:26 +0300

    adjusted some parts in the code

--- site.js
+++ site.js
@@ -1,21 +1,8 @@
-// let isChatbotInViewport = () => {
-//     const chatbot_block = document.getElementById('chatbot_block');
-//     const rect = chatbot_block.getBoundingClientRect();
-//     return (
-//         rect.top >= 0 &&
-//         rect.left >= 0 &&
-//         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-//         rect.right<=(window.innerWidth|| document.documentElement.clientwidth)
-//     )
-// }
-// let toggleChatbotVisibility = () => {
-//     const chatbot_block = document.getElementById('chatbot_block');
-//     if (isChatbotInViewport()) {
-//         chatbot_block.style.display = 'block';
-//     } else { 
-//         chatbot_block.style.display='none';
-//     }
-// }
-// window.addEventListener('resize', toggleChatbotVisibility);
-// window.addEventListener('scroll', toggleChatbotVisibility);
-// toggleChatbotVisibility();
+window.addEventListener('scroll', function () {
+    const chatBlock = document.getElementById('chatbot_block');
+    const rect = chatBlock.getBoundingClientRect();
+    //  check if the chatBlock is within the viewport
+    if (rect.top>=0 && rect.bottom <= window.innerHeight) {
+        chatBlock.style.display = 'block';
+    } else { chatBlock.style.display = 'none';}   
+});
