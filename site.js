808456b5f6b71a6c38eaaf6a93ab8fc2b7aeb640
Daniel Kibe <148968967+DanKibee@users.noreply.github.com>
2024-11-03 19:04:13 +0300

    updating some parts of the web

--- site.js
+++ site.js
@@ -1,8 +1,26 @@
-window.addEventListener('scroll', function () {
-    const chatBlock = document.getElementById('chatbot_block');
-    const rect = chatBlock.getBoundingClientRect();
-    //  check if the chatBlock is within the viewport
-    if (rect.top>=0 && rect.bottom <= window.innerHeight) {
-        chatBlock.style.display = 'block';
-    } else { chatBlock.style.display = 'none';}   
+window.addEventListener("scroll", function () {
+  const chatBlock = document.getElementById("chatbot_block");
+  const rect = chatBlock.getBoundingClientRect();
+  //  check if the chatBlock is within the viewport
+  if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+    chatBlock.style.display = "block";
+  } else {
+    chatBlock.style.display = "none";
+  }
+});
+const image= document.getElementById("img1");
+const hoverimage = document.getElementById("squareblock");
+image.addEventListener("mouseover", function () {
+  hoverimage.style.display = "block";
+});
+image.addEventListener("mouseout", function () {
+  hoverimage.style.display = "none";
+});
+const image2 = document.getElementById("img2");
+const hoverimage2 = document.getElementById("squareblock2");
+image2.addEventListener("mouseover", function () {
+  hoverimage2.style.display = "block";
+});
+image2.addEventListener("mouseout", function () {
+  hoverimage2.style.display = "none";
 });
