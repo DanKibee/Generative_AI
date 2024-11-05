84c85dc2e4160de605a621b924a89c46d2c5b554
Daniel Kibe <148968967+DanKibee@users.noreply.github.com>
2024-11-05 11:02:36 +0300

    added a viewport function

--- site.js
+++ site.js
@@ -1,13 +1,4 @@
-window.addEventListener("scroll", function () {
-  const chatBlock = document.getElementById("chatbot_block");
-  const rect = chatBlock.getBoundingClientRect();
-  //  check if the chatBlock is within the viewport
-  if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-    chatBlock.style.display = "block";
-  } else {
-    chatBlock.style.display = "none";
-  }
-});
+
 const image= document.getElementById("img1");
 const hoverimage = document.getElementById("squareblock");
 image.addEventListener("mouseover", function () {
@@ -24,3 +15,42 @@ image2.addEventListener("mouseover", function () {
 image2.addEventListener("mouseout", function () {
   hoverimage2.style.display = "none";
 });
+const image3 = document.getElementById("img3");
+const chat = document.getElementById("pixlr");
+
+window.addEventListener('scroll', e => {
+for (i = 0; i < p.length; i++) {
+  b = p;
+  console.log(b);
+  }
+})
+const p = document.getElementsByClassName("disappear");
+
+function elementInViewport(el) {
+  var top = el.offsetTop;
+  var left = el.offsetLeft;
+  var width = el.offsetWidth;
+  var height = el.offsetHeight;
+ 
+  while (el.offsetParent) {
+    el = el.offsetParent;
+    top += el.offsetTop;
+    left += el.offsetLeft;
+  }
+  return (
+    top >= window.pageYOffset &&
+    left >= window.pageXOffset &&
+    top + height <= window.pageYOffset + window.innerHeight &&
+    left+ width <=window.pageXOffset + window.innerWidth
+  )
+}
+window.addEventListener("scroll", (el) => { 
+  console.log(el)
+  for (let i = 0; i < p.length; i++) { 
+    if (elementInViewport(p[i])) {
+      p[i].style.display = 'block';
+    } else { 
+      p[i].style.display = 'none';
+    }
+  }
+})
