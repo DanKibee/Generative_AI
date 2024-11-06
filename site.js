f3cc78ac724704ddc555d5757199589733a08642
Daniel Kibe <148968967+DanKibee@users.noreply.github.com>
2024-11-07 02:04:21 +0300

    hover

--- site.js
+++ site.js
@@ -1,39 +1,26 @@
-let image = document.getElementsByClassName("disappear");
-let Arrayimage = Array.from(image);
-for (let i = 0; i < Array.length; i++) {
-  Arrayimage.addEventListener("mouseover", function () {
-    id = Arrayimage[i].id;
-    if (id == "gemini") {
-      document.getElementById("squareblock2")[i].innerHTML =
-        "Gemini, formerly known as Bard, is a generative artificial intelligence chatbot developed by Google.< br > Wikipedia ";
-    } else if (id == "pixlr") {
-    }
-  });
-}
-// const image= document.getElementById("img1");
-// const hoverimage = document.getElementById("squareblock");
-// image.addEventListener("mouseover", function () {
-//   hoverimage.style.display = "block";
-// });
-// image.addEventListener("mouseout", function () {
-//   hoverimage.style.display = "none";
-// });
-// const image2 = document.getElementById("img2");
-// const hoverimage2 = document.getElementById("squareblock2");
-// image2.addEventListener("mouseover", function () {
-//   hoverimage2.style.display = "block";
-// });
-// image2.addEventListener("mouseout", function () {
-//   hoverimage2.style.display = "none";
-// });
-// const image3 = document.getElementById("img3");
-// const chat = document.getElementById("pixlr");
-
-window.addEventListener("scroll", (e) => {
-  for (i = 0; i < p.length; i++) {
-    b = p;
-    console.log(b);
-  }
+const image = document.getElementById("img1");
+const hoverimage = document.getElementById("squareblock");
+image.addEventListener("mouseover", function () {
+  hoverimage.style.display = "block";
+});
+image.addEventListener("mouseout", function () {
+  hoverimage.style.display = "none";
+});
+const image2 = document.getElementById("img2");
+const hoverimage2 = document.getElementById("squareblock2");
+image2.addEventListener("mouseover", function () {
+  hoverimage2.style.display = "block";
+});
+image2.addEventListener("mouseout", function () {
+  hoverimage2.style.display = "none";
+});
+const image3 = document.getElementById("img3");
+const hoverimage3 = document.getElementById("pixlr");
+image3.addEventListener("mouseover", function () {
+  hoverimage3.style.display = "block";
+});
+image3.addEventListener("mouseout", function () {
+  hoverimage3.style.display = "none";
 });
 const p = document.getElementsByClassName("disappear");
 
@@ -59,9 +46,9 @@ window.addEventListener("scroll", (el) => {
   console.log(el);
   for (let i = 0; i < p.length; i++) {
     if (elementInViewport(p[i])) {
-      p[i].style.display = "block";
+      p[i].style["z-index"][i] = "1";
     } else {
-      p[i].style.display = "none";
+      p[i].style["z-index"] = "-1";
     }
   }
 });
